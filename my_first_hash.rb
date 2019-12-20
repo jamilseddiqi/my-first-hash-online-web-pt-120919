e8e5fdbb8cbdff8583d777e4c826c82bed4584a1
Jamil Seddiqi <jamilsediq@gmail.com>
2019-12-20 14:36:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,44 +35,46 @@ def adding
   shipping_manifest["muskets"] = 2
 
   shipping_manifest["gun powder"] = 4
+  
+
 
 
 end
 
 
-# _______________________________
+# # _______________________________
 
-def my_hash 
-my_hash = {"name" => "Jamil"}
+# def my_hash 
+# my_hash = {"name" => "Jamil"}
 
-end 
+# end 
 
-def the_manifest
+# def the_manifest
   
-the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+# the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
-end 
+# end 
 
-def retrieval
+# def retrieval
 
-shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                }
+# shipping_manifest = {
+#                   "whale bone corsets" => 5, 
+#                   "porcelain vases" => 2, 
+#                   "oil paintings" => 3
+#                 }
                  
-shipping_manifest["oil paintings"]
-end 
+# shipping_manifest["oil paintings"]
+# end 
 
-def adding 
+# def adding 
 
-shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                }
+# shipping_manifest = {
+#                   "whale bone corsets" => 5, 
+#                   "porcelain vases" => 2, 
+#                   "oil paintings" => 3
+#                 }
 
-shipping_manifest["muskets"] = 2
+# shipping_manifest["muskets"] = 2
 
-shipping_manifest["gun powder"] = 4
-end 
+# shipping_manifest["gun powder"] = 4
+# end 
